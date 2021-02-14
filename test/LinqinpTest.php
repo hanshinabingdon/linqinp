7cf8f249a86f24f493d1db0c947b376a0a90c830
taito <hanshin.abingdon@gmail.com>
2021-02-14 23:46:17 +0900

    add test

---
+++ test/LinqinpTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace LinqinpTest;
+
+use Linqinp\Linqinp;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class LinqinpTest
+ * @package LinqinpTest
+ */
+class LinqinpTest extends TestCase
+{
+
+}
